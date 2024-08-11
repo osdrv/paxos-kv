fa2d09076991713568c5499b74468b1017ded5d9
Oleg Sidorov <oleg@sidorov.nl>
2024-08-11 21:48:57 +0200

    Implement put/get API

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,78 @@
-mod command;
-mod network;
-mod paxos_message;
-mod paxos_node;
-mod server;
+pub mod command;
+pub mod network;
+pub mod paxos_message;
+pub mod paxos_node;
+pub mod server;
 
 use crate::paxos_node::PaxosNode;
 use crate::server::{send_paxos_message, start_server};
 use std::sync::{Arc, Mutex};
 use tokio::task;
 
+fn read_topology(path: &str) -> Vec<String> {
+    let file = std::fs::read_to_string(path).expect("Failed to read the file");
+    file.lines().map(|s| s.trim().to_string()).collect()
+}
+
 #[tokio::main]
 async fn main() {
-    let node1 = Arc::new(Mutex::new(PaxosNode::new(1)));
-    let node2 = Arc::new(Mutex::new(PaxosNode::new(2)));
+    let topology = read_topology("topology.txt");
+    let node_addr = std::env::args().nth(1).expect("Expected a node address");
+    let control_addr = std::env::args().nth(2).expect("Expected a control address");
 
-    let node1_addr = "127.0.0.1:8081";
-    let node2_addr = "127.0.0.1:8082";
+    let node_ix = topology
+        .iter()
+        .position(|x| x == &node_addr)
+        .expect("Node not found in topology") as u64;
 
-    task::spawn(start_server(Arc::clone(&node1), node1_addr));
-    task::spawn(start_server(Arc::clone(&node2), node2_addr));
+    let node = Arc::new(Mutex::new(PaxosNode::new(node_ix)));
+    let jh_srv = task::spawn(start_server(Arc::clone(&node), node_addr));
+    let jh_control_srv = task::spawn(server::start_control_server(
+        Arc::clone(&node),
+        control_addr,
+        topology,
+    ));
 
-    let prepare_message =
-        node1
-            .lock()
-            .unwrap()
-            .start_proposal(1, "key".to_string(), "value".to_string());
-    println!("Sending a prepare message");
-    if let Some(promise_response) =
-        send_paxos_message(Arc::clone(&node1), node2_addr, prepare_message)
-            .await
-            .unwrap()
-    {
-        if let Some(accept_message) = node1.lock().unwrap().handle_promise(promise_response) {
-            if let Some(accepted_response) =
-                send_paxos_message(Arc::clone(&node1), node2_addr, accept_message)
-                    .await
-                    .unwrap()
-            {
-                node1.lock().unwrap().handle_accepted(accepted_response);
-            } else {
-                println!("Failed to handle accepted");
-            }
-        } else {
-            println!("Failed to handle promise");
-        }
-    } else {
-        println!("Failed to send proposal request");
-    }
+    jh_srv.await.unwrap();
+    jh_control_srv.await.unwrap();
+
+    // let jh = task::spawn();
+    // jh.await.unwrap();
+
+    // let node1 = Arc::new(Mutex::new(PaxosNode::new(1)));
+    // let node2 = Arc::new(Mutex::new(PaxosNode::new(2)));
+
+    // let node1_addr = "127.0.0.1:8081";
+    // let node2_addr = "127.0.0.1:8082";
+
+    // task::spawn(start_server(Arc::clone(&node1), node1_addr));
+    // task::spawn(start_server(Arc::clone(&node2), node2_addr));
+
+    // let prepare_message =
+    //     node1
+    //         .lock()
+    //         .unwrap()
+    //         .start_proposal(1, "key".to_string(), "value".to_string());
+    // println!("Sending a prepare message");
+    // if let Some(promise_response) =
+    //     send_paxos_message(Arc::clone(&node1), node2_addr, prepare_message)
+    //         .await
+    //         .unwrap()
+    // {
+    //     if let Some(accept_message) = node1.lock().unwrap().handle_promise(promise_response) {
+    //         if let Some(accepted_response) =
+    //             send_paxos_message(Arc::clone(&node1), node2_addr, accept_message)
+    //                 .await
+    //                 .unwrap()
+    //         {
+    //             node1.lock().unwrap().handle_accepted(accepted_response);
+    //         } else {
+    //             println!("Failed to handle accepted");
+    //         }
+    //     } else {
+    //         println!("Failed to handle promise");
+    //     }
+    // } else {
+    //     println!("Failed to send proposal request");
+    // }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,113 @@
+use crate::command::Command;
 use crate::network::{receive_message_with_retry, send_message_with_retry};
 use crate::paxos_message::PaxosMessage;
 use crate::paxos_node::PaxosNode;
 
+use std::str::FromStr;
 use std::sync::{Arc, Mutex};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 
 const MAX_RETRIES: u32 = 5;
 
-pub async fn start_server(node: Arc<Mutex<PaxosNode>>, addr: &str) -> tokio::io::Result<()> {
+async fn node_get(
+    node: Arc<Mutex<PaxosNode>>,
+    topology: Vec<String>,
+    key: String,
+) -> tokio::io::Result<Option<String>> {
+    // TODO: implement the get operation
+    println!("TODO: GET key={}", key);
+    return Ok(None);
+}
+
+async fn node_put(
+    node: Arc<Mutex<PaxosNode>>,
+    topology: Vec<String>,
+    key: String,
+    value: String,
+) -> tokio::io::Result<()> {
+    // TODO: implement the put operation
+    println!("TODO: PUT key={} value={}", key, value);
+    return Ok(());
+}
+
+pub async fn handle_command(
+    stream: TcpStream,
+    node: Arc<Mutex<PaxosNode>>,
+    topology: Vec<String>,
+) -> tokio::io::Result<()> {
+    let mut reader = BufReader::new(stream);
+    let mut buffer = String::new();
+
+    loop {
+        buffer.clear();
+        let bytes_read = reader.read_line(&mut buffer).await?;
+        if bytes_read == 0 {
+            break;
+        }
+        let node = Arc::clone(&node);
+
+        match Command::from_str(&buffer) {
+            Ok(Command::Get(key)) => {
+                let result = node_get(node, topology.clone(), key).await;
+                let response = match result {
+                    Ok(Some(value)) => value,
+                    Ok(None) => "".to_string(),
+                    Err(e) => format!("Error: {:?}", e),
+                };
+                reader.write_all(response.as_bytes()).await?;
+                reader.write_all(b"\n").await?;
+            }
+            Ok(Command::Put(key, value)) => {
+                //let result = node.lock().unwrap().put(key, value);
+                let result = node_put(node, topology.clone(), key, value).await;
+                let response = match result {
+                    Ok(_) => "".to_string(),
+                    Err(e) => format!("Error: {:?}", e),
+                };
+                reader.write_all(response.as_bytes()).await?;
+                reader.write_all(b"\n").await?;
+            }
+            Ok(Command::Invalid(command)) => {
+                let response = format!("Invalid command: {}", command);
+                reader.write_all(response.as_bytes()).await?;
+                reader.write_all(b"\n").await?;
+            }
+            Err(_) => {
+                let response = "Error parsing command".to_string();
+                reader.write_all(response.as_bytes()).await?;
+                reader.write_all(b"\n").await?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn start_control_server(
+    node: Arc<Mutex<PaxosNode>>,
+    control_addr: String,
+    topology: Vec<String>,
+) -> tokio::io::Result<()> {
+    let listener = TcpListener::bind(control_addr.as_str()).await?;
+    println!("Started control server on {}", control_addr);
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let node = Arc::clone(&node);
+        let topology = topology.clone();
+
+        tokio::spawn(async move {
+            if let Err(r) = handle_command(stream, node, topology).await {
+                println!("Error handling command: {:?}", r);
+            }
+        });
+    }
+}
+
+pub async fn start_server(node: Arc<Mutex<PaxosNode>>, addr: String) -> tokio::io::Result<()> {
     println!("Starting node {:?} at {:?}", node, addr);
-    let listener = TcpListener::bind(addr).await?;
+    let listener = TcpListener::bind(addr.as_str()).await?;
     println!("Started a listener at {:?}", addr);
 
     loop {
@@ -64,4 +162,17 @@ pub async fn send_paxos_message(
     let response = receive_message_with_retry(&mut stream, MAX_RETRIES).await?;
     println!("received a response: {:?}", response);
     Ok(Some(response))
+}
+
+impl FromStr for Command {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Command, Self::Err> {
+        let parts: Vec<&str> = input.trim().splitn(3, ' ').collect();
+        match parts.as_slice() {
+            ["get", key] => Ok(Command::Get(key.to_string())),
+            ["put", key, value] => Ok(Command::Put(key.to_string(), value.to_string())),
+            _ => Ok(Command::Invalid(input.to_string())),
+        }
+    }
 }
